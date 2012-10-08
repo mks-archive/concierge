4ca921157c6e3ba120a1653c3713dff064447fab
Mike Schinkel <mikeschinkel@gmail.com>
2012-10-08 02:13:33 -0400

    Remove the code to clear child categories; we don't need it after previous change.

--- examples/foursquare/venue-categories.js
+++ examples/foursquare/venue-categories.js
@@ -2,7 +2,6 @@ $api.GET('venue-categories',function(data){
 	var category, categories = {};
 	for(var i=0; i<data.response.categories.length; i++){
 		category = data.response.categories[i];
-		category.categories = [];	// Clear child categories
 		categories[category.id] = category.name;
 	}
 	$api.out( categories );
