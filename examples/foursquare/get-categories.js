05f2934a1ade093a3fd5996124162967e9a94205
Mike Schinkel <mikeschinkel@gmail.com>
2012-10-08 00:01:50 -0400

    Created a much more interesting example showing the transform of a complete result set to a simple result set.

--- examples/foursquare/get-categories.js
+++
@@ -1,4 +0,0 @@
-$api.GET('venue-categories',function(data){
-	$api.out( data );
-});
-
--- examples/foursquare/venue-categories.js
+++ examples/foursquare/venue-categories.js
@@ -1,4 +1,3 @@
-// This is a FourSquare Get Categories API
 $api.GET('venue-categories',function(data){
 	var category, categories = {};
 	for(var i=0; i<data.response.categories.length; i++){
